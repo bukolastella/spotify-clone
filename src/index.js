9ea34709616669634879944ca10e00b4358b6a2f
Buks <abolarinbukolastella@gmail.com>
2021-07-17 15:00:32 +0100

    browser router added

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/index";
 
 ReactDOM.render(
-  <HashRouter basename="/">
+  <BrowserRouter>
     <Provider store={store}>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </Provider>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
